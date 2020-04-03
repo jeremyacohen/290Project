05c41a5898dab28ea123827392e8766a66c888da
jeremy.cohen <jeremy.cohen@duke.edu>
2020-04-03 16:07:33 -0700

    Initial Commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by Jeremy Cohen on 4/3/20.
+//  Copyright © 2020 Jeremy Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
